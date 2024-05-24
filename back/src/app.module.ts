60af77782ad5e695433d2cd35ca9aa6e8b6808a2
jihun <hotchapa@naver.com>
2024-05-25 01:12:50 +0900

    📦️Chore : GitHub Actions 워크 플로우 설정 완료

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,19 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './user/entities/user.entity';
 import { UsersModule } from './user/user.module';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 전역 모듈로 설정
+    }),
     TypeOrmModule.forRoot({
-      type: 'mysql', // 데이터베이스 타입
-      host: '127.0.0.1', // 데이터베이스 호스트 (Docker 사용 시 도커 컴포즈 서비스 이름 사용)
-      port: 3306, // MySQL 포트
-      username: 'root', // 사용자 이름
-      password: '1234', // 비밀번호
-      database: 'test', // 데이터베이스 이름
-      entities: [User], // 엔티티 파일 위치
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User],
       synchronize: true, // 개발용으로만 true로 설정, 프로덕션에서는 false 권장
     }),
     UsersModule,
