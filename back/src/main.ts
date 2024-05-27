285784ab099fa66aa6a1eb17674f9ff479a2bd46
jihun <hotchapa@naver.com>
2024-05-28 01:16:48 +0900

    🚑Fix : 깃헙 액션 워크 플로우 수정

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000, '0.0.0.0'); // 모든 네트워크 인터페이스에서 접속을 허용
 }
