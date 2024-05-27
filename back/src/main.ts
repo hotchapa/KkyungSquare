a59c24285fd50081fcb1bcba4747cd040aebe051
jihun <hotchapa@naver.com>
2024-05-28 00:10:15 +0900

    🚑Fix : main.ts 수정 (모든 네트워크 허용)

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0'); // 모든 네트워크 인터페이스에서 접속을 허용
 }
 bootstrap();
