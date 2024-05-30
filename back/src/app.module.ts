ac0849e8b11d18ed8328a520240a8d321fd45b09
jihun <hotchapa@naver.com>
2024-05-31 01:33:09 +0900

    🚑Fix : typeORM 설정 변경

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './user/entities/user.entity';
@@ -11,15 +11,19 @@ import { UsersModule } from './user/user.module';
     ConfigModule.forRoot({
       isGlobal: true, // 전역 모듈로 설정
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [User],
-      synchronize: true, // 개발용으로만 true로 설정, 프로덕션에서는 false 권장
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [User],
+        synchronize: true, // 개발용으로만 true로 설정, 프로덕션에서는 false 권장
+      }),
     }),
     UsersModule,
   ],
