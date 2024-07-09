10c3ddf7e4c5a8a712acbb9b13137d8ab5eac08a
jihun <hotchapa@naver.com>
2024-07-09 23:50:52 +0900

    📦️Chore : 메타데이터 변경

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -3,7 +3,7 @@ import StyledComponentsRegistry from '@/common/libs/registry'
 
 export const metadata: Metadata = {
   title: '뀽스퀘어',
-  description: '뀽카페에 어서오세요.',
+  description: '뀽스퀘어에 어서오세요.',
 }
 
 export default function RootLayout({
