c5e6948e110bd93ef8ecc875fc363c844d082c30
jihun <hotchapa@naver.com>
2024-08-03 18:05:06 +0900

    🔥Remove: back 폴더 삭제

--- back/init.sql
+++
@@ -1,3 +0,0 @@
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';
-ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASSWORD}';
-FLUSH PRIVILEGES;
