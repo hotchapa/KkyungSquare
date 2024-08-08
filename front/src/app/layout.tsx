3ae516bb110e60994f3bfd0d883f398aa532ece4
jihun <hotchapa@naver.com>
2024-08-08 19:01:12 +0900

    💄Design: 루트 레이아웃 및 페이지 스타일링

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,10 +1,19 @@
 import type { Metadata } from 'next'
 import './style.scss'
 import { ThemeProvider } from 'next-themes'
+import TopNavbar from '@/common/components/top-navbar'
+import localFont from 'next/font/local'
+
 export const metadata: Metadata = {
   title: '뀽스퀘어',
   description: '뀽스퀘어에 어서오세요.',
 }
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+})
 
 export default function RootLayout({
   children,
@@ -13,8 +22,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
+      <body className={pretendard.className}>
+        <ThemeProvider>
+          <TopNavbar />
+          <div className='child'>
+          {children}
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,12 @@
-import './page.scss'
+import styles from './page.module.scss'
 import ThemeSwitch from '@/common/libs/theme-button'
 export default function Home() {
   return (
     <div>
       <ThemeSwitch />
-      <section className="dd">와우맨</section>
-      <section className="dd">와우맨</section>
-      <section className="dd">와우맨</section>
+      <section className={styles.dd}>와우맨</section>
+      <section className={styles.dd}>와우맨</section>
+      <section className={styles.dd}>와우맨</section>
     </div>
   )
 }
