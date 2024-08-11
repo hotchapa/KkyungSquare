3027bc3bae4d1de3d85abea1f3cd1e4030482fda
jihun <hotchapa@naver.com>
2024-08-11 22:43:14 +0900

    🚀Feat: MSW 핸들러 작성 및 회원가입 서버액션 작성

--- front/src/mocks/handlers.ts
+++ front/src/mocks/handlers.ts
@@ -10,11 +10,38 @@ function generateDate() {
   })
 }
 
-const User = []
 const Posts = []
 const delay = (ms: number) =>
   new Promise((res) => {
     setTimeout(res, ms)
   })
 
-export const handlers = []
+export const handlers = [
+  // http.post('/api/login', () => {
+  //   console.log('로그인')
+  //   return HttpResponse.json(User[1], {
+  //     headers: {
+  //       'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/',
+  //     },
+  //   })
+  // }),
+  http.post('/api/logout', () => {
+    console.log('로그아웃')
+    return new HttpResponse(null, {
+      headers: {
+        'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0',
+      },
+    })
+  }),
+  http.post('/api/account/sign-up', async ({ request }) => {
+    console.log('회원가입')
+    // return HttpResponse.text(JSON.stringify('user_exists'), {
+    //   status: 403,
+    // })
+    return HttpResponse.text(JSON.stringify('ok'), {
+      headers: {
+        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/',
+      },
+    })
+  }),
+]
