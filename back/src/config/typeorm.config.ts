edd1bb1bd7f0b2372c8d6ed40a57eb5ed6b283b3
jihun <hotchapa@naver.com>
2024-08-16 22:22:41 +0900

    🚀Feat: Auth 모듈, 서비스, 컨트롤러, 레포 생성 및 로컬 MySQL TypeORM 설정

---
+++ back/src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'zmfmdjgm0',
+  database: 'kkyung_local_db',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
